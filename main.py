3fd564dce52cc69735cdd508432029a8baee5a47
jackwhite001 <1121176166@qq.com>
2023-12-27 10:47:19 +0800

    'python代码测试'

---
+++ main.py
@@ -0,0 +1,10 @@
+# 主函数，导入 utils 函数ku
+
+from utils.fun1 import *
+
+
+def test():
+    obj = ['aaa','sadsad','sadsad222','33333www']
+    loopPrintFun(obj)
+
+test()
---
+++ utils/fun1.py
@@ -0,0 +1,7 @@
+
+# 创建函数功能库
+
+def loopPrintFun(obj):
+    for (step,num) in enumerate(obj):
+        print('遍历第个对象%d，对象值为%s'%(step,num));
+    print('循环打印完成.................');
